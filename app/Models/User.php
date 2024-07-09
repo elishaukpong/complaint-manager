e71a6571267a1f45ae761f0bbfe592551d90d837
Elisha Ukpong <ishukpong418@gmail.com>
2024-07-09 15:12:53 +0100

    added side bar

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
